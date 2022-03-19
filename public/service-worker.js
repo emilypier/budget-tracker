4566b66c025dd4e5105a9702e43692afb09be093
emilypier <piersoee@miamioh.edu>
2022-03-18 22:55:41 -0400

    deploying to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const APP_PREFIX = 'FoodEvent-';     
-const VERSION = 'version_01';
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_02';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   '/',
